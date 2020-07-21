40fa4c0e223f656988b46b29470ee5bb2520659e
Rostislav Kondratenko <r.kondratenko@wwpass.com>
2020-07-21 17:29:45 +0300

    Rewrite websocket to use a onePool per instace classe and a common Promise that resolves on success for ano of the tickets.

--- src/qrcode/wwpass.websocket.js
+++ src/qrcode/wwpass.websocket.js
@@ -1,143 +1,107 @@
 import { WWPASS_OK_MSG, WWPASS_STATUS } from '../passkey/constants';
-import navigateToCallback from '../navigation';
 
-const connectionPool = [];
-const closeConnectionPool = () => {
-  while (connectionPool.length) {
-    const connection = connectionPool.shift();
-    if (connection.readyState === WebSocket.OPEN) {
-      connection.close();
+class websocketPool {
+  constructor(options) {
+    this.connectionPool = [];
+    const defaultOptions = {
+      spfewsAddress: 'wss://spfews.wwpass.com',
+      clientKeyOnly: false,
+      log: () => {}
+    };
+    this.options = { ...defaultOptions, ...options };
+    this.promise = new Promise((resolve, reject) => {
+      this.resolve = resolve;
+      this.reject = reject;
+    });
+  }
+
+  onError(status, reason, ticket) {
+    this.reject({
+      status,
+      reason,
+      ticket
+    });
+    this.close();
+  }
+
+  close() {
+    while (this.connectionPool.length) {
+      const connection = this.connectionPool.shift();
+      if (connection.readyState === WebSocket.OPEN) {
+        connection.close();
+      }
     }
   }
-};
 
-const applyDefaults = (initialOptions) => {
-  const defaultOptions = {
-    ppx: 'wwp_',
-    version: 2,
-    ticket: undefined,
-    callbackURL: undefined,
-    returnErrors: false,
-    log: () => {},
-    development: false,
-    spfewsAddress: 'wss://spfews.wwpass.com',
-    echo: undefined,
-    clientKeyOnly: false
-  };
-  return { ...defaultOptions, ...initialOptions };
-};
-/**
-* WWPass SPFE WebSocket connection
-* @param {object} options
-*
-* options = {
-*   'ticket': undefined, // stirng
-*   'callbackURL': undefined, //string
-*   'development': false || 'string' , // work with another spfews.wwpass.* server
-*   'log': function (message) || console.log, // another log handler
-*   'echo': undefined
-* }
-*/
-const getWebSocketResult = (initialOptions) => new Promise((resolve, reject) => {
-  const options = applyDefaults(initialOptions);
-  let clientKey = null;
-  let originalTicket = options.ticket;
-  let ttl = null;
-  const settle = (status, reason) => {
-    if (status === 200) {
-      const result = {
-        ppx: options.ppx,
-        version: options.version,
-        status,
-        reason: WWPASS_OK_MSG,
-        ticket: options.ticket,
-        callbackURL: options.callbackURL,
-        clientKey,
-        originalTicket,
-        ttl
-      };
-      if (!options.clientKeyOnly) {
-        navigateToCallback(result);
-      }
-      resolve(result);
-    } else {
-      const err = {
-        ppx: options.ppx,
-        version: options.version,
-        status,
-        reason,
-        ticket: options.ticket,
-        callbackURL: options.callbackURL
-      };
-      if ((status === WWPASS_STATUS.INTERNAL_ERROR || options.returnErrors)
-            && !options.clientKeyOnly) {
-        navigateToCallback(err);
-      }
-      reject(err);
+  watchTicket(ticket) {
+    if (!('WebSocket' in window)) {
+      this.onError(WWPASS_STATUS.INTERNAL_ERROR, 'WebSocket is not supported.', ticket);
+      return;
     }
-  };
-  if (!('WebSocket' in window)) {
-    settle(WWPASS_STATUS.INTERNAL_ERROR, 'WebSocket is not supported.');
-    return;
-  }
-  const websocketurl = options.spfewsAddress;
-  const socket = new WebSocket(websocketurl);
-  connectionPool.push(socket);
-  const { log } = options;
+    const socket = new WebSocket(this.options.spfewsAddress);
+    this.connectionPool.push(socket);
+    const { log } = this.options;
+    let clientKey = null;
+    let originalTicket = null;
+    let ttl = null;
 
-  socket.onopen = () => {
-    try {
-      log(`Connected: ${websocketurl}`);
-      const message = JSON.stringify({ ticket: options.ticket });
-      log(`Sent message to server: ${message}`);
-      socket.send(message);
-    } catch (error) {
-      log(error);
-      settle(WWPASS_STATUS.INTERNAL_ERROR, 'WebSocket error');
-    }
-  };
-
-  socket.onclose = () => {
-    try {
-      const index = connectionPool.indexOf(socket);
-      if (index !== -1) {
-        connectionPool.splice(index, 1);
+    socket.onopen = () => {
+      try {
+        log(`Connected: ${this.options.spfewsAddress}`);
+        const message = JSON.stringify({ ticket });
+        log(`Sent message to server: ${message}`);
+        socket.send(message);
+      } catch (error) {
+        log(error);
+        this.onError(WWPASS_STATUS.INTERNAL_ERROR, 'WebSocket error', ticket);
       }
-      log('Disconnected');
-      resolve({ refresh: true });
-    } catch (error) {
-      log(error);
-      settle(WWPASS_STATUS.INTERNAL_ERROR, 'WebSocket error');
-    }
-  };
+    };
 
-  socket.onmessage = (message) => {
-    try {
-      log(`Message received from server: ${message.data}`);
-      const response = JSON.parse(message.data);
-      const status = response.code;
-      const { reason } = response;
-
-      if ('clientKey' in response && !clientKey) {
-        clientKey = response.clientKey;
-        if (response.originalTicket !== undefined) {
-          originalTicket = response.originalTicket;
-          ttl = response.ttl;
+    socket.onclose = () => {
+      try {
+        const index = this.connectionPool.indexOf(socket);
+        if (index !== -1) {
+          this.connectionPool.splice(index, 1);
         }
+        log('Disconnected');
+      } catch (error) {
+        log(error);
+        this.onError(WWPASS_STATUS.INTERNAL_ERROR, 'WebSocket error', ticket);
       }
+    };
 
-      if (status === 100) { return; }
-      settle(status, reason);
-      socket.close();
-    } catch (error) {
-      log(error);
-      settle(WWPASS_STATUS.INTERNAL_ERROR, 'WebSocket error');
-    }
-  };
-});
+    socket.onmessage = (message) => {
+      try {
+        log(`Message received from server: ${message.data}`);
+        const response = JSON.parse(message.data);
+        const status = response.code;
 
+        if ('clientKey' in response && !clientKey) {
+          clientKey = response.clientKey;
+          if (response.originalTicket !== undefined) {
+            originalTicket = response.originalTicket;
+            ttl = response.ttl;
+          }
+        }
 
-export {
-  getWebSocketResult,
-  closeConnectionPool
-};
+        if (status !== 200) {
+          return; // Skip all errors. Nothing to do about them
+        }
+        this.resolve({
+          status,
+          reason: WWPASS_OK_MSG,
+          clientKey,
+          ticket,
+          ttl,
+          originalTicket: originalTicket !== null ? originalTicket : ticket
+        });
+        this.close();
+      } catch (error) {
+        log(error);
+        this.onError(WWPASS_STATUS.INTERNAL_ERROR, 'WebSocket error');
+      }
+    };
+  }
+}
+
+export default websocketPool;
