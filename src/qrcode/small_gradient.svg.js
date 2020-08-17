84b90ccd379d4d8dd0d052ad7cf133c27c25612e
Rostislav Kondratenko <r.kondratenko@wwpass.com>
2020-08-17 21:17:57 +0300

    Update small_gradient.svg to include white rim

--- src/qrcode/small_gradient.svg.js
+++ src/qrcode/small_gradient.svg.js
@@ -1,17 +1,17 @@
-const qrCodeLogoSVG = `
-  <defs>
-    <linearGradient id="gr1" x1="0" y1="0" x2="1" y2="0">
-      <stop offset="0" stop-color="#115fff"/>
-      <stop offset="0.06" stop-color="#007fff"/>
-      <stop offset="0.2" stop-color="#00d1c5"/>
-      <stop offset="0.33" stop-color="#00ff29"/>
-      <stop offset="0.5" stop-color="#dbff00"/>
-      <stop offset="0.67" stop-color="#00ff29"/>
-      <stop offset="0.8" stop-color="#00d1c5"/>
-      <stop offset="0.94" stop-color="#007fff"/>
-      <stop offset="1" stop-color="#115fff"/>
-    </linearGradient>
-  </defs>
-  <rect width="100" height="100" style="fill:url(#gr1)"/>`;
+const qrCodeLogoSVG = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="100" viewBox="0 0 100 100">
+<defs>
+  <linearGradient id="gra">
+    <stop offset="0.05" stop-color="#007fff"/>
+    <stop offset="0.2" stop-color="#00d1c5"/>
+    <stop offset="0.34" stop-color="#00ff29"/>
+    <stop offset="0.5" stop-color="#dbff00"/>
+    <stop offset="0.66" stop-color="#00ff29"/>
+    <stop offset="0.8" stop-color="#00d1c5"/>
+    <stop offset="0.95" stop-color="#007fff"/>
+  </linearGradient>
+</defs>
+<path d="M100,0H0V100H100Z" fill="#fff"/>
+<rect x="9" y="9" width="82" height="82" rx="4" fill="url(#gra)" />
+</svg>`;
 
 export default qrCodeLogoSVG;
